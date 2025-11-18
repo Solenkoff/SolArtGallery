3162887e40825c75184976c2adf2a6043387247a
Solenkoff <solenkoff@yahoo.com>
2025-11-18 13:43:51 +0200

    Add Gallery Catalog

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
 import Footer from './components/footer/Footer'
+import GalleryCatalog from './components/gallery/GalleryCatalog'
 import Header from './components/header/Header'
 import Home from './components/home/Home'
 
@@ -9,6 +10,7 @@ function App() {
         <>
             <Header />
             <Home />
+            <GalleryCatalog/>
             <Footer />
         </>
     )
