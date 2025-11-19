f1bf25d10c2d43b7a8c5e0676c047cb5296d8af8
Solenkoff <solenkoff@yahoo.com>
2025-11-19 12:09:25 +0200

    Add ItemDetails

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import AuthorList from './components/author/AuthorList'
 import ContactUs from './components/contact/ContactUs'
 import Footer from './components/footer/Footer'
 import GalleryCatalog from './components/gallery/GalleryCatalog'
+import ItemDetails from './components/gallery/ItemDetails'
 import Header from './components/header/Header'
 import Home from './components/home/Home'
 import LoginUser from './components/user/LoginUser'
@@ -16,6 +17,7 @@ function App() {
             <ContactUs />
             <Home />
             <GalleryCatalog/>
+            <ItemDetails />
             <AuthorList />
             <LoginUser />
             <RegisterUser />
