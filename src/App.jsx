ed35180931436bbffaeccd450b9de3904d3f6541
Solenkoff <solenkoff@yahoo.com>
2025-11-24 16:55:29 +0200

    Add react-transition-group for component transitions

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
-import { Route, Routes } from "react-router-dom";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
+import { Route, Routes, useLocation } from "react-router-dom";
+import { useRef } from "react";
+
 import ContextProvider from "./context/ContextProvider"
 
 import Home from './components/home/Home'
@@ -10,32 +13,47 @@ import ItemDetails from './components/gallery/ItemDetails'
 import AuthorList from './components/author/AuthorList'
 import ContactUs from './components/contact/ContactUs'
 import Footer from './components/footer/Footer'
+import Logout from "./components/user/Logout";
 
 import './App.css'
 
 function App() {
+    const location = useLocation();
+    const nodeRef = useRef(null);
 
     return (
         <ContextProvider>
             <Header />
-            <Routes>
+            <TransitionGroup>
+                <CSSTransition
+                    key={location.pathname}
+                    timeout={300}
+                    classNames="slide"
+                    unmountOnExit
+                    nodeRef={nodeRef}
+                >
+                    <div ref={nodeRef}>
+                        <Routes>
 
-                <Route path="/" element={<Home />} />
-                <Route path="/contact" element={<ContactUs />} />
-                <Route path="/authors" element={<AuthorList />} />
+                            <Route path="/" element={<Home />} />
+                            <Route path="/contact" element={<ContactUs />} />
+                            <Route path="/authors" element={<AuthorList />} />
 
+                            <Route path="/gallery">
+                                <Route path="" element={<GalleryCatalog />} />
+                                <Route path="details/:itemId" element={<ItemDetails />} />
+                            </Route>
 
-                <Route path="/gallery">
-                    <Route path="" element={<GalleryCatalog />} />
-                    <Route path="details/:itemId" element={<ItemDetails />} />
-                </Route>
+                            <Route path="/users">
+                                <Route path="login" element={<Login />} />
+                                <Route path="register" element={<Register />} />
+                                <Route path="logout" element={<Logout />} />
+                            </Route>
 
-                <Route path="/users">      // !!!
-                    <Route path="login" element={<Login />} />
-                    <Route path="register" element={<Register />} />
-                </Route>
-
-            </Routes>
+                        </Routes>
+                    </div>
+                </CSSTransition>
+            </TransitionGroup>
             <Footer />
         </ContextProvider>
     )
